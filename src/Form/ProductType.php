f29bf0e21b42eec730e55111be42d1a30aa253b2
Stefanut99009 <stefanutpazargic@gmail.com>
2024-11-17 12:06:22 +0200

    Initial commit

---
+++ src/Form/ProductType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('is_active', CheckboxType::class, [
+                'label' => 'Active', // Optional, displays a label for the checkbox
+                'required' => false,
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
